307e2bfd7d9e4de55eef5041e5d8ee98bf9ef483
zlflshdh84 <60860605+zlflshdh84@users.noreply.github.com>
2021-04-06 14:46:09 +0900

    SignupPage 회원가입 부분 수정중
    - SignupPage Database 연동해서 데이터 축적까지 완료
    - 중복검사와 페이지 이동 작성 필요.

---
+++ html/public/firebaseSetting.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+   apiKey: "AIzaSyAV9us9GilCgU8wRgbrHLV69NN0jyhflVc",
+   authDomain: "communityproject-ab6f4.firebaseapp.com",
+   databaseURL: "https://communityproject-ab6f4-default-rtdb.firebaseio.com",
+   projectId: "communityproject-ab6f4",
+   storageBucket: "communityproject-ab6f4.appspot.com",
+   messagingSenderId: "13978519495",
+   appId: "1:13978519495:web:e8a2385e91a9d07ff6bbe2",
+   measurementId: "G-ET5LMZYKXZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
